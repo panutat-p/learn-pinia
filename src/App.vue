401585f85763ba4ff036114911af6074709ce1f0
panutat-p <pp.panutat@gmail.com>
2023-03-20 01:48:16 +0700

    feat: apply shopping cart boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import TheHeader from './components/TheHeader.vue';
+import ProductCard from './components/ProductCard.vue';
+
+const products = ref([
+  {
+    name: "Plain Ol' Pineapple",
+    image: 'pineapple-original.jpg',
+    price: 4,
+  },
+  {
+    name: 'Dried Pineapple',
+    image: 'pineapple-dried.jpg',
+    price: 5,
+  },
+  {
+    name: 'Pineapple Gum',
+    image: 'pineapple-gum.jpg',
+    price: 3,
+  },
+  {
+    name: 'Pineapple T-Shirt',
+    image: 'pineapple-tshirt.jpg',
+    price: 12,
+  },
+]);
+</script>
 
 <template>
-  <div class="flex items-center justify-center h-screen">
-    <h1 class="text-3xl">Hello</h1>
+  <div class="container">
+    <TheHeader />
+    <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
+      <ProductCard v-for="product in products" :key="product.name" :product="product" />
+    </ul>
   </div>
 </template>
